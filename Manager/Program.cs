17886bb5c77272d885ab948b268f4215b7f550c0
Atara57769 <atara57769@gmail.com>
2025-11-18 19:00:11 +0200

    Add project files.

---
+++ Manager/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Manager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = "Data Source=ATARA;Initial Catalog=ApiDB;Integrated Security=True;Encrypt=False";
+            //int numRoW=DB.InsertProducts(connectionString);
+            //Console.WriteLine($"rows effected {numRoW}");
+            //DB.printProducts(connectionString);
+            int numRoW1 = DB.InsertCategories(connectionString);
+            Console.WriteLine($"rows effected {numRoW1}");
+            DB.printCategories(connectionString);
+
+        }
+    }
+}
